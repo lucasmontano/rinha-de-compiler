d4744b66f636f4a6c6b00ec334d79fd3a1aad61c
Lucas Montano <lucasmontano@gmail.com>
2023-09-24 18:16:31 +0200

    adding missing BinaryOp

--- interpreter.js
+++ interpreter.js
@@ -86,6 +86,20 @@ function interpret(node, environment) {
                     return lhs < rhs;
                 case 'Eq':
                     return lhs === rhs;
+                case 'Neq':
+                    return lhs !== rhs;
+                case 'Gt':
+                    return lhs > rhs;
+                case 'Gte':
+                    return lhs >= rhs;
+                case 'Lte':
+                    return lhs <= rhs;
+                case 'And':
+                    return lhs && rhs;
+                case 'Or':
+                    return lhs || rhs;
+                case 'Rem':
+                    return lhs % rhs;
                 default:
                     return console.error(`Unknown operator ${node.op}`);
             }
