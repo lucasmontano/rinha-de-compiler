f502759d872caa7d78c73e2adadb83bcf801d838
Lucas Montano <lucasmontano@gmail.com>
2023-09-24 15:35:14 +0200

    adding Var, If, BinaryOp Eq

--- interpreter.js
+++ interpreter.js
@@ -1,8 +1,44 @@
+const matMemo = {};
+
+function matMul(A, B) {
+    return [
+        [A[0][0] * B[0][0] + A[0][1] * B[1][0], A[0][0] * B[0][1] + A[0][1] * B[1][1]],
+        [A[1][0] * B[0][0] + A[1][1] * B[1][0], A[1][0] * B[0][1] + A[1][1] * B[1][1]]
+    ];
+}
+
+function matKey(matrix, n) {
+    return `${matrix[0][0]}-${matrix[0][1]}-${matrix[1][0]}-${matrix[1][1]}-${n}`;
+}
+
+function matPow(matrix, n) {
+    const key = matKey(matrix, n);
+    if (matMemo[key]) {
+        return matMemo[key];
+    }
+
+    if (n === 1n) {
+        matMemo[key] = matrix;
+        return matrix;
+    }
+
+    let result;
+    if (n % 2n === 0n) {
+        const halfPow = matPow(matrix, n / 2n);
+        result = matMul(halfPow, halfPow);
+    } else {
+        result = matMul(matrix, matPow(matrix, n - 1n));
+    }
+
+    matMemo[key] = result;
+    return result;
+}
+
 function interpret(node, environment) {
     switch (node.kind) {
         case 'Call':
             // Fibonacci Ninja Interpret            
-            if (node.callee.text === 'fib') {
+            if (node.callee.text === 'fib-IGNORA-BRINCADEIRA') {
 
                 // Confused? -> https://youtu.be/XfmZRS6oP3U?si=4iAyLD2SCFBRB9d0
                 const n = BigInt(interpret(node.arguments[0], environment));
@@ -20,17 +56,17 @@ function interpret(node, environment) {
                     return result[1][0].toString();
                 }
             } else {
-                const callee = interpret(node.callee, environment);
+                const { node: calleeNode, environment: calleeEnvironment } = interpret(node.callee, environment);
                 const args = node.arguments.map(arg => interpret(arg, environment));
-                const newEnvironment = { ...environment };
-                callee.parameters.forEach((param, index) => {
-                    newEnvironment[param] = args[index];
+                const newEnvironment = { ...calleeEnvironment };
+                calleeNode.parameters.forEach((param, index) => {
+                    newEnvironment[param.text] = args[index];
                 });
-                return interpret(callee.value, newEnvironment);
-            }            
+                return interpret(calleeNode.value, newEnvironment);
+            }
         case 'Int':
             return node.value;
-        case 'Binary':       
+        case 'Binary':
             const lhs = interpret(node.lhs, environment);
             const rhs = interpret(node.rhs, environment);
             switch (node.op) {
@@ -48,15 +84,17 @@ function interpret(node, environment) {
                     return lhs / rhs;
                 case 'Lt':
                     return lhs < rhs;
+                case 'Eq':
+                    return lhs === rhs;
                 default:
                     return console.error(`Unknown operator ${node.op}`);
-            }    
+            }
         case 'Function':
-            return node;
+            return { node, environment };
         case 'Let':
             const value = interpret(node.value, environment);
             environment[node.name.text] = value;
-            return interpret(node.next, { ...environment, [node.name.text]: value });        
+            return interpret(node.next, { ...environment, [node.name.text]: value });
         case 'Str':
             return node.value;
         case 'Print':
@@ -67,28 +105,24 @@ function interpret(node, environment) {
                     return term;
                 case 'string':
                     console.log(term);
-                    return term;            
+                    return term;
+            }
+        case 'Var':
+            const newVar = node.text
+            if (newVar in environment) {
+                return environment[newVar];
+            } else {
+                return console.error(`Variable ${newVar} not defined`);
+            }
+        case 'If':
+            const condition = interpret(node.condition, environment);
+            if (condition) {
+                return interpret(node.then, environment);
+            } else {
+                return interpret(node.otherwise, environment);
             }
         default:
             return console.error(`Unknown node kind: ${node.kind}`);
-    }
-
-    // Helper function to multiply 2x2 matrices
-    function matMul(A, B) {
-        return [
-            [A[0][0]*B[0][0] + A[0][1]*B[1][0], A[0][0]*B[0][1] + A[0][1]*B[1][1]],
-            [A[1][0]*B[0][0] + A[1][1]*B[1][0], A[1][0]*B[0][1] + A[1][1]*B[1][1]]
-        ];
-    }
-
-    // Helper function to power a 2x2 matrix
-    function matPow(matrix, n) {
-        if (n === 1n) return matrix;
-        if (n % 2n === 0n) {
-            const halfPow = matPow(matrix, n / 2n);
-            return matMul(halfPow, halfPow);
-        }
-        return matMul(matrix, matPow(matrix, n - 1n));
     }
 }
 
